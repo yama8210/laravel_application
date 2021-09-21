b94206ff565b867f93d42bd2c3ac70d381894b51
山崎睦美 <yamazakimutsumi@yamazakimutsuminoMacBook-Air.local>
2021-09-21 14:14:28 +0900

    課題11

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\PostRequest;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index',['posts'=>$posts]);
+    }
+    public function create(){
+        return view('posts.create');
+    }
+    public function store(PostRequest $request)
+    {
+        $post = new Post;
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->user_id = Auth::id();
+
+        $post->save();
+    
+        return redirect()->route('post.index');
+    }
+    public function show($id)
+    {
+    
+        $post = Post::findOrFail($id);
+
+        return view('posts.show', ['post' => $post]);
+
+    }
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+
+        if ($post->user_id !== Auth::id()){
+            return redirect('/');
+        }
+        dd($post);
+    
+        return view('posts.edit',['post' => $post]);
+    }
+    
+    public function update(PostRequest $request,$id)
+    {
+        $post = Post::findOrFail($id);
+        
+        if ($post->user_id !== Auth::id()){
+            return redirect('/');
+        }
+    
+        $post->title = $request->title;
+        $post->body = $request->body;
+
+        $post->save();
+
+        return redirect('/');
+    }
+    public function delete($id)
+    {
+    
+        $post = Post::findOrFail($id);
+
+        if ($post->user_id !== Auth::id()) {
+            return redirect('/');
+        }
+
+        $post->delete(); 
+
+        return redirect('/');
+    }
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('sample', 'SampleController@index')->name('sample');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'PostController@index')->name('post.index');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('post/create', 'PostController@create')->name('post.create');
+    Route::post('post/create', 'PostController@store')->name('post.store');
+    Route::get('post/{id}/show', 'PostController@show')->name('post.show');
+    Route::get('post/{id}/edit', 'PostController@edit')->name('post.edit'); // 追加
+    Route::post('post/{id}/update', 'PostController@update')->name('post.update');
+});
